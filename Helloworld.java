0a7ebd9989f8b40640fd9c0324ece76f44e8d969
sanjudarla <127183530+sanjudarla@users.noreply.github.com>
2023-03-11 16:44:39 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld
 {
  public static void main(String[] args)
  {
-   System.out.println("**********Trying jenkins with webhooks*************");
+   System.out.println("**********Trying jenkins with webhookss*************");
 
  }
 }
